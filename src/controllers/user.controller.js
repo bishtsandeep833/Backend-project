2fe28d4ad85a575e6b05c8276fd6ea5de1fadb4b
Sandeep Bisht <bishtsandeep833@gmail.com>
2026-07-14 12:59:38 +0530

    Add subscription model

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -165,7 +165,7 @@ const logoutUser = asyncHandler( async (req, res) => {
     .clearCookie("accessToken", options)
     .clearCookie("refreshToken", options)
     .json(new ApiResponse(200, {}, "User logged out successfully"))
-})
+})   
 
 const refreshAccessToken = asyncHandler( async(req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
