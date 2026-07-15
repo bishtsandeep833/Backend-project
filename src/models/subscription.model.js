58d2a5fcc9bf75a32f6a9a27ca28ccbeca783572
Sandeep Bisht <bishtsandeep833@gmail.com>
2026-07-15 16:06:04 +0530

    creates subscription

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 
+
 const subscriptionSchema = new Schema({
   subscriber: {
     type: Schema.Types.ObjectId, // One who is subscribing 
@@ -11,5 +12,6 @@ const subscriptionSchema = new Schema({
   },
 
 },{timestamps: true})
+
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
